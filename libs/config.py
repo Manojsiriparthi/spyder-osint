5d87dd0548c69a8b9b35b855d10074af79822f97
Manoj Siriparthi <manojs.devops1@gmail.com>
2025-11-01 23:41:50 +0530

    updated

--- libs/config.py
+++ libs/config.py
@@ -7,3 +7,31 @@ shodan_api_key = ""
 fullcontact_api_key = ""
 virus_total_api_key = ""
 email_hunter_api_key = ""
+
+# Basic configuration for Spyder OSINT
+
+# Color codes
+red = '\033[91m'
+green = '\033[92m'
+yellow = '\033[93m'
+blue = '\033[94m'
+end = '\033[0m'
+
+# Status indicators
+info = f'{blue}[i]{end}'
+good = f'{green}[+]{end}'
+bad = f'{red}[-]{end}'
+run = f'{yellow}[~]{end}'
+
+# Global settings
+verbose = False
+
+# Intelligence sources
+INTELS = [
+    'github.com',
+    'pastebin.com',
+    'twitter.com',
+    'facebook.com',
+    'linkedin.com',
+    'instagram.com'
+]
--- libs/utils.py
+++ libs/utils.py
@@ -1,20 +1,19 @@
+import json
+
 """
 Utils functions -
 """
 
+def format_dict(data):
+    """Format dictionary for display"""
+    return json.dumps(data, indent=2)
 
-def format_dict(data, attribute=None):
-    for key, value in data.items():
-        if key != attribute:
-            if isinstance(value, dict):
-                if any(value.values()):
-                    print(f'{key.capitalize()}::')
-                    for sub_key, sub_value in value.items():
-                        if sub_value:
-                            print(f'\t{sub_key.capitalize()}:: {sub_value}')
-            elif isinstance(value, list):
-                if value:
-                    print(f'{key.capitalize()}:: {", ".join(value) if isinstance(value[0], str) else value}')
-            else:
-                if value is not None:
-                    print(f'{key.capitalize()}:: {value}')
+def proxy_type(proxy_string):
+    """Parse proxy string into dictionary format"""
+    if ':' in proxy_string:
+        host, port = proxy_string.split(':', 1)
+        return {
+            'http': f'http://{host}:{port}',
+            'https': f'https://{host}:{port}'
+        }
+    return None
